d437fee3cfcbcceb684ab773f8e7d7c37cb83a6c
trimonvl <trimon@telenet.be>
2016-12-08 20:18:18 +0100

    Enemy Spawn

--- src/gui/geowars.java
+++ src/gui/geowars.java
@@ -33,6 +33,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JFrame;
 import org.dyn4j.collision.manifold.Manifold;
@@ -55,231 +56,238 @@ import org.dyn4j.geometry.Vector2;
  */
 public class geowars extends simulationPanel {
 
-    /**
-     * The serial version id
-     */
-    private static final long serialVersionUID = -313391186714427055L;
-    
+	/**
+	 * The serial version id
+	 */
+	private static final long serialVersionUID = -313391186714427055L;
 
-    /**
-     * Default constructor for the canvas
-     * Creates the base of a game
-     */
-    public geowars() {
-        //creates a simulation panel with a scale of 1
-        super(1);
+	/**
+	 * Default constructor for the canvas Creates the base of a game
+	 */
+	public geowars() {
+		// creates a simulation panel with a scale of 1
+		super(1);
 
-        //add key and collisionListener
-        KeyListener listener = new CustomKeyListener();
-        this.addKeyListener(listener);
-        MouseListener mouse = new CustomMouseListener();
-        this.addMouseListener(mouse);
-        this.addMouseMotionListener((MouseMotionListener) mouse);
-        this.world.addListener(new CustomCollisionListener());
-        this.requestFocus();
-        
-        //temp for testing enemy shooting
-        this.rand = new Random();
-    }
-    
-    //temp vars for ship and enemy for testing
-    private Ship ship;
-    private Enemy enemy;
+		// add key and collisionListener
+		KeyListener listener = new CustomKeyListener();
+		this.addKeyListener(listener);
+		MouseListener mouse = new CustomMouseListener();
+		this.addMouseListener(mouse);
+		this.addMouseMotionListener((MouseMotionListener) mouse);
+		this.world.addListener(new CustomCollisionListener());
+		this.requestFocus();
 
-    //privates for controlling player action
-    private boolean moveLeft;
-    private boolean moveRight;
-    private boolean moveUp;
-    private boolean moveDown;
-    private boolean firing;
-    
-    private Random rand;
-    
-    
-    private class CustomCollisionListener extends CollisionAdapter {
-        @Override
-        public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1) {
-            boolean cont = true;
-            if(body instanceof SimulationBody && body1 instanceof SimulationBody) {
-                    SimulationBody b = (SimulationBody) body;
-                    SimulationBody b1 = (SimulationBody) body1;
-                    b.isHit();
-                    b1.isHit();
-                    if(b.isDead() && !b.getMass().isInfinite()) {
-                        world.removeBody(b);
-                        cont = false;
-                    }
-                    if(b1.isDead() && !b1.getMass().isInfinite()) {
-                        world.removeBody(b1);
-                        cont = false;
-                    }
-            }
-            return false;
+		// temp for testing enemy shooting
+		this.rand = new Random();
+	}
 
-        }
-        @Override
-        public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1, Penetration pntrtn) {
-            System.out.println("penetration collision");
-            return true;
-        }
+	// temp vars for ship and enemy for testing
+	private Ship ship;
+	private ArrayList<Enemy> enemyList;
 
-        @Override
-        public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1, Manifold mnfld) {
-            System.out.println("manifold collision");
-            return true;
-        }
+	// privates for controlling player action
+	private boolean moveLeft;
+	private boolean moveRight;
+	private boolean moveUp;
+	private boolean moveDown;
+	private boolean firing;
 
-        @Override
-        public boolean collision(ContactConstraint cc) {
-            System.out.println("contactconstraint collision");    
-            return true;
-        }    
-    }
+	private Random rand;
 
-    private class CustomMouseListener extends MouseAdapter {
-        @Override
-        public void mousePressed(MouseEvent e) {
-           firing = true;
-        }
-        
-        @Override
-        public void mouseReleased(MouseEvent e) {
-           firing = false;
-        }
-        
-        @Override
-        public void mouseMoved(MouseEvent e) {
-            Point p = e.getPoint();
-            ship.turnToAngle(new Vector2(p.getX(),p.getY()));
-        }
-    }
-    
-    private class CustomKeyListener extends KeyAdapter {
+	private class CustomCollisionListener extends CollisionAdapter {
+		@Override
+		public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1) {
+			boolean cont = true;
+			if (body instanceof SimulationBody && body1 instanceof SimulationBody) {
+				SimulationBody b = (SimulationBody) body;
+				SimulationBody b1 = (SimulationBody) body1;
+				b.isHit();
+				b1.isHit();
+				if (b.isDead() && !b.getMass().isInfinite()) {
+					world.removeBody(b);
+					cont = false;
+				}
+				if (b1.isDead() && !b1.getMass().isInfinite()) {
+					world.removeBody(b1);
+					cont = false;
+				}
+			}
+			return false;
 
-        @Override
-        public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
-                    moveLeft = true;
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    moveRight = true;
-                    break;
-                case KeyEvent.VK_UP:
-                    moveUp = true;
-                    break;
-                case KeyEvent.VK_DOWN:
-                    moveDown = true;
-                    break;
-            }
+		}
 
-        }
+		@Override
+		public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1, Penetration pntrtn) {
+			System.out.println("penetration collision");
+			return true;
+		}
 
-        @Override
-        public void keyReleased(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
-                    moveLeft = false;
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    moveRight = false;
-                    break;
-                case KeyEvent.VK_UP:
-                    moveUp = false;
-                    break;
-                case KeyEvent.VK_DOWN:
-                    moveDown = false;
-                    break;
-            }
-        }
-    }
+		@Override
+		public boolean collision(Body body, BodyFixture bf, Body body1, BodyFixture bf1, Manifold mnfld) {
+			System.out.println("manifold collision");
+			return true;
+		}
 
-    /**
-     * Creates game objects and adds them to the world.
-     */
-    protected void initializeWorld() {
-        // set gravity to none :) welcome to space.
-        this.world.setGravity(new Vector2(0, 0));
-        // the floor
-        SimulationBody floor = new SimulationBody();
-        floor.addFixture(Geometry.createRectangle(1024, 1));
-        floor.setMass(MassType.INFINITE);
-        floor.translate(512, 767);
-        this.world.addBody(floor);
-        
-        SimulationBody ceiling = new SimulationBody();
-        ceiling.addFixture(Geometry.createRectangle(1024, 1));
-        ceiling.setMass(MassType.INFINITE);
-        ceiling.translate(512, 0);
-        this.world.addBody(ceiling);
+		@Override
+		public boolean collision(ContactConstraint cc) {
+			System.out.println("contactconstraint collision");
+			return true;
+		}
+	}
 
-        // some bounding shapes
-        SimulationBody right = new SimulationBody();
-        right.addFixture(Geometry.createRectangle(1, 768));
-        right.setMass(MassType.INFINITE);
-        right.translate(1023, 768/2);
-        this.world.addBody(right);
-        SimulationBody left = new SimulationBody();
-        left.addFixture(Geometry.createRectangle(1, 768));
-        left.setMass(MassType.INFINITE);
-        left.translate(0, 768/2);
-        this.world.addBody(left);
-        
-        ship = new Ship();
-        this.world.addBody(ship);
-        enemy = new Enemy(5);
-        this.world.addBody(enemy);
-    }
+	private class CustomMouseListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			firing = true;
+		}
 
-    /* (non-Javadoc)
-	 * @see org.dyn4j.samples.SimulationFrame#update(java.awt.Graphics2D, double)
-     */
-    @Override
-    protected void update(Graphics2D g, double elapsedTime) {
-        // do player action based on key/mouse input
-        if (moveLeft) {
-            ship.move(-50,0);
-        }
-        if (moveRight) {
-            ship.move(50,0);
-        }
-        if (moveUp) {
-            ship.move(0,-50);
-        }
-        if (moveDown) {
-            ship.move(0,50);
-        }
-        if (firing && !ship.isDead()) {
-            this.world.addBody(ship.shoot());
-            firing = false;
-        }
-        ship.turnToAngle();
-        
-        if(!enemy.isDead()){
-            enemy.move(ship.getWorldCenter());
-            if(rand.nextInt(10) == 5) {
-                this.world.addBody(enemy.shoot());
-            }
-        }
-        
-        super.update(g, elapsedTime);
-       
-        System.out.println(this.world.getBodyCount());
-    }
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			firing = false;
+		}
 
-    /**
-     * Entry point for the example application.
-     *
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        JFrame temp = new JFrame("geowars");
-        temp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        geowars simulation = new geowars();
-        temp.add(simulation);
-        temp.pack();
-        temp.setVisible(true);
-        simulation.setFocusable(true);
-        simulation.run();
-    }
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			Point p = e.getPoint();
+			ship.turnToAngle(new Vector2(p.getX(), p.getY()));
+		}
+	}
+
+	private class CustomKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				moveLeft = true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				moveRight = true;
+				break;
+			case KeyEvent.VK_UP:
+				moveUp = true;
+				break;
+			case KeyEvent.VK_DOWN:
+				moveDown = true;
+				break;
+			}
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				moveLeft = false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				moveRight = false;
+				break;
+			case KeyEvent.VK_UP:
+				moveUp = false;
+				break;
+			case KeyEvent.VK_DOWN:
+				moveDown = false;
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Creates game objects and adds them to the world.
+	 */
+	protected void initializeWorld() {
+		// set gravity to none :) welcome to space.
+		this.world.setGravity(new Vector2(0, 0));
+		// the floor
+		SimulationBody floor = new SimulationBody();
+		floor.addFixture(Geometry.createRectangle(1024, 1));
+		floor.setMass(MassType.INFINITE);
+		floor.translate(512, 767);
+		this.world.addBody(floor);
+
+		SimulationBody ceiling = new SimulationBody();
+		ceiling.addFixture(Geometry.createRectangle(1024, 1));
+		ceiling.setMass(MassType.INFINITE);
+		ceiling.translate(512, 0);
+		this.world.addBody(ceiling);
+
+		// some bounding shapes
+		SimulationBody right = new SimulationBody();
+		right.addFixture(Geometry.createRectangle(1, 768));
+		right.setMass(MassType.INFINITE);
+		right.translate(1023, 768 / 2);
+		this.world.addBody(right);
+		SimulationBody left = new SimulationBody();
+		left.addFixture(Geometry.createRectangle(1, 768));
+		left.setMass(MassType.INFINITE);
+		left.translate(0, 768 / 2);
+		this.world.addBody(left);
+
+		ship = new Ship();
+		this.world.addBody(ship);
+		enemyList = new ArrayList<>();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.dyn4j.samples.SimulationFrame#update(java.awt.Graphics2D,
+	 * double)
+	 */
+	@Override
+	protected void update(Graphics2D g, double elapsedTime) {
+		// do player action based on key/mouse input
+		if (moveLeft) {
+			ship.move(-50, 0);
+		}
+		if (moveRight) {
+			ship.move(50, 0);
+		}
+		if (moveUp) {
+			ship.move(0, -50);
+		}
+		if (moveDown) {
+			ship.move(0, 50);
+		}
+		if (firing && !ship.isDead()) {
+			this.world.addBody(ship.shoot());
+			firing = false;
+		}
+		ship.turnToAngle();
+
+		for (Enemy enemy : enemyList) {
+			if (!enemy.isDead()) {
+				enemy.move(ship.getWorldCenter());
+				if (rand.nextInt(10) == 5) {
+					//this.world.addBody(enemy.shoot());
+				}
+			}
+		}
+		if(rand.nextInt(200)==1)
+		{
+			enemyList.add(new Enemy(3));
+			this.world.addBody(enemyList.get(enemyList.size()-1));
+		}
+		super.update(g, elapsedTime);
+
+		System.out.println(this.world.getBodyCount());
+	}
+
+	/**
+	 * Entry point for the example application.
+	 *
+	 * @param args
+	 *            command line arguments
+	 */
+	public static void main(String[] args) {
+		JFrame temp = new JFrame("geowars");
+		temp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		geowars simulation = new geowars();
+		temp.add(simulation);
+		temp.pack();
+		temp.setVisible(true);
+		simulation.setFocusable(true);
+		simulation.run();
+	}
 }

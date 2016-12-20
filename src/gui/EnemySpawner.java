30504e3c0bf3e641f93a5941ed22e6e5e7046cde
Jonas Lauwers <jonas.lauwers@gmail.com>
2016-12-20 18:31:40 +0200

    added assets and image implementation basics

--- src/gui/EnemySpawner.java
+++ src/gui/EnemySpawner.java
@@ -1,30 +1,48 @@
- package gui;
+package gui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EnemySpawner {
-	private int speed;
-	private int xPos;
-	private int yPos;
-	private String type;
-	public EnemySpawner(int speed, int xPos, int yPos, String type) {
-		this.speed = speed;
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.type = type;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	public int getSpeed() {
-		return speed;
-	}
-	public int getxPos() {
-		return xPos;
-	}
-	public int getyPos() {
-		return yPos;
-	}
-	public String getType() {
-		return type;
-	}
-	
+
+    private static final String DRAGON = "/assets/Enemies/Enemy-DragonShip-1.png";
+    private static final String KING = "/assets/Enemies/Enemy-KingsShip-1.png";
+    private static final String WARRIOR = "/assets/Enemies/Enemy-WarriorShip-1.png";
+    
+    private int speed;
+    private int xPos;
+    private int yPos;
+    private String type;
+
+    public EnemySpawner(int speed, int xPos, int yPos, String type) {
+        this.speed = speed;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        try {
+            this.type = (String) EnemySpawner.class.getDeclaredField(type.toUpperCase()).get(null);
+        }
+        catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException ex) {
+            this.type = "";
+        }
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
